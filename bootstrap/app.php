ef17499e422d2540b4091e0078717e7713d417f6
Mub <mubpersonal5@gmail.com>
2024-05-20 18:32:51 -0500

    Plantilla admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,10 +6,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        // web: __DIR__.'/../routes/web.php',
+        // api: __DIR__.'/../routes/api.php',
+        // commands: __DIR__.'/../routes/console.php',
+        // health: '/up',
+        using: function (Illuminate\Routing\Router $router) {
+            $router->middleware("web")
+                ->group(base_path("routes/web.php"));
+            $router->middleware("web")
+                ->prefix("admin")
+                ->group(base_path("routes/admin.php"));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
